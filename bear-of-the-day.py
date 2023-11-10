b19795c5621f899c3712f3e4f8ff86abee0723e8
David <davidhand1988@gmail.com>
2023-11-09 22:24:31 -0500

    Add CI/CD pipeline and AWS Lambda handler

--- bear-of-the-day.py
+++ bear-of-the-day.py
@@ -12,28 +12,27 @@ import send_email
 # The image will then be saved to an S3 bucket and emailed to the user.
 def bear_of_the_day():
     # load the style CSV file into an array
-    #These are all wrong, they're all coming in as dicts and not arrays
     subjects = load_data_file('subjects.csv')
     styles = load_data_file('styles.csv')
     scenes = load_data_file('scenes.csv')
 
-    #randomly select a style
+    # randomly select a style
     subject = random.choice(subjects)
     style = random.choice(styles)
     scene = random.choice(scenes)
     prompt = subject + " " + scene + " in the style of " + style 
     print(prompt)
-    #generate the image
+    # generate the image
     image_url = dalle.generate_image(prompt)
     print(image_url)
 
-    #save the image to a file with the timestamp
+    # save the image to a file with the timestamp
     image_path = 'static/images/' + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + '.jpg'
     download_image(image_url, image_path)
 
-    #save the image to S3
-    #s3.save_image_to_s3(image_url)
-    #email the image to the user
+    # save the image to S3
+    # s3.save_image_to_s3(image_url)
+    # email the image to the user
     recipients = os.environ['RECIPIENTS'].split(',')
     print("Sending email to " + str(recipients) + "...")
     send_email.send_image_email(recipients, image_path)
@@ -48,6 +47,14 @@ def download_image(url, file_path):
     response = requests.get(url)
     with open(file_path, 'wb') as f:
         f.write(response.content)
+
+# AWS Lambda handler
+def lambda_handler(event, context):
+    try:
+        bear_of_the_day()
+    except Exception as e:
+        print(f"Error: {e}")
+        raise e
 
 if __name__ == "__main__":
     load_dotenv()
